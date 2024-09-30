3969e98b2ffc17d4a16ccc41f8546d46b41a19b9
Henshingly <dietmar_kersting@web.de>
2024-09-30 09:15:12 +0200

    Aktualisieren von acp_stopforumspam.php
    better german wording

--- language/de/acp/acp_stopforumspam.php
+++ language/de/acp/acp_stopforumspam.php
@@ -75,9 +75,9 @@ $lang = array_merge($lang, [
 	'PAYPAL_IMAGE_URL'				=> 'https://www.paypalobjects.com/webstatic/en_US/i/btn/png/silver-pill-paypal-26px.png',
 	'PAYPAL_ALT'					=> 'Spende über PayPal',
 	'BUY_ME_A_BEER_URL'				=> 'https://paypal.me/RMcGirr83',
-	'BUY_ME_A_BEER'					=> 'Kauf mir ein Bier, für die Erstellung dieser Erweiterung',
+	'BUY_ME_A_BEER'					=> 'Für die Erstellung dieser Erweiterung gib mir doch ein Bier aus',
 	'BUY_ME_A_BEER_SHORT'			=> 'Spende für diese Erweiterung',
-	'BUY_ME_A_BEER_EXPLAIN'			=> 'Diese Erweiterung ist völlig kostenlos. Es ist ein Projekt, mit dem ich meine Zeit zum Vergnügen und zur Nutzung der phpBB-Community verbringe. Wenn Du diese Erweiterung gerne verwendest oder Dein Forum davon profitiert hat, denke doch bitte daran <a href="https://paypal.me/RMcGirr83" target="_blank" rel="noreferrer noopener">mir ein Bier zu kaufen</a>. Ich wäre sehr dankbar. <i class="fa fa-smile-o" style="color:green;font-size:1.5em;" aria-hidden="true"></i>',
+	'BUY_ME_A_BEER_EXPLAIN'			=> 'Diese Erweiterung ist völlig kostenlos. Es ist ein Projekt, mit dem ich meine Zeit zum Vergnügen und zur Nutzung der phpBB-Community verbringe. Wenn Du diese Erweiterung gerne verwendest oder Dein Forum davon profitiert hat, denke doch bitte daran <a href="https://paypal.me/RMcGirr83" target="_blank" rel="noreferrer noopener">mir ein Bier auszugeben</a>. Dafür wäre ich sehr dankbar. <i class="fa fa-smile-o" style="color:green;font-size:1.5em;" aria-hidden="true"></i>',
 	'SFS_CONTACTADMIN_EXT'			=> 'Erweiterung "Kontakt zum Admin" zulassen',
 	'SFS_CONTACTADMIN_EXT_EXPLAIN'	=> 'Wenn Ja ausgewählt und die Erweiterung "Kontakt zum Admin" installiert ist, wird diese Erweiterung in die Erweiterung "Kontakt zum Admin" integriert.'
 ]);
